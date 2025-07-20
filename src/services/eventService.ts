1ca9ed75111ad5136e4e53bfcebdeabd88c18d93
TUMIT_HASAN <tumithasan1@gmail.com>
2025-07-20 07:44:57 +0600

    create all apis on one from

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -2,3 +2,28 @@ import axios from 'axios';
 import { Event } from '../types/Event';
 
 const API_URL = 'http://localhost:5000/events';
+
+// for fetch all function
+export const fetchEvents = async (): Promise<Event[]> => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+// add new evenet 
+
+export const addEvent = async (newEvent: Omit<Event, 'id'>): Promise<Event> => {
+  const response = await axios.post(API_URL, newEvent);
+  return response.data;
+};
+
+
+
+export const archiveEvent = async (id: string): Promise<Event> => {
+  const response = await axios.put(`${API_URL}/${id}`);
+  return response.data;
+};
+
+
+export const deleteEvent = async (id: string): Promise<void> => {
+  await axios.delete(`${API_URL}/${id}`);
+};
